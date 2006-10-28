902b182f83259fe5dcd9b8eae40880c4604f6213
qode <qode@c754d5e9-d413-0410-bcdf-9574c9dba6b7>
2006-10-28 01:12:39 +0000

    Added FastImageApp to experimental branch..
    Moved the fastimagetool from the wrong branch (legacy) to the correct one (experimental)

--- branches/experimental/libsecondlife-cs/Utils/FastImageTool.cs
+++ branches/experimental/libsecondlife-cs/Utils/FastImageTool.cs
@@ -1,16 +1,45 @@
+/*
+ * Copyright (c) 2006, Second Life Reverse Engineering Team
+ * All rights reserved.
+ *
+ * - Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * - Neither the name of the Second Life Reverse Engineering Team nor the names
+ *   of its contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+//See FastImageApp for an example! :)
+//Please excuse my mess :/
 using System;
 using System.Collections;
 using System.Text;
 using libsecondlife;
 using libsecondlife.Packets;
 
-namespace FastImageTool
+namespace libsecondlife.Utils.FastImageTool
 {
     public delegate void ImageFinishedCallback(LLUUID id, byte[] data); //this delegate is called when an image completed.
 
     public class Image
     {
-        public static void debug(Object message)
+        public static void debug(string message)
         {
             Console.WriteLine("FIT:IMAGE DEBUG -> " + message);
         }
@@ -111,7 +140,7 @@ namespace FastImageTool
 
     public class ImageManager
     {
-        public static void debug(Object message)
+        public static void debug(string message)
         {
             Console.WriteLine("FIT:IMAGEMANAGER DEBUG -> " + message);
         }
@@ -200,6 +229,7 @@ namespace FastImageTool
             if (ims.Count == 0) return; //no images to update at this time, go ahead and exit.
             RequestImagePacket rip = new RequestImagePacket();
 
+
             //go ahead and do the silly security stuff in this packet
             rip.AgentData.AgentID = client.Network.AgentID;
             rip.AgentData.SessionID = client.Network.SessionID;
@@ -228,6 +258,7 @@ namespace FastImageTool
             }
             catch
             {
+                //shh...
                 return;
             }
 
